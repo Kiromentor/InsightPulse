e154d8f6bb16b978266825a8067a746bec8c2e43
ueatmil18 <silverfenix@msn.com>
2025-07-19 00:45:08 -0300

    Fix: import directo de analyzer

--- analyzer.py
+++ analyzer.py
@@ -1,5 +1,7 @@
 # analysis/analyzer.py
 from textblob import TextBlob
+import nltk nltk.download('punkt')  # Solo si hace falta (Render puede fallar sin esto)
+
 
 def analyze_text(text):
     blob = TextBlob(text)
